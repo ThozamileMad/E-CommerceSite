ca8f667198bd8ca0475302a9f47868013bed1ea2
ThozamileMad <118363197+ThozamileMad@users.noreply.github.com>
2024-07-17 13:49:10 +0200

    Add files via upload

--- static/scripts/sign_up_scripts.js
+++ static/scripts/sign_up_scripts.js
@@ -1,21 +1,72 @@
 const form = document.getElementById("form")
-const userInputs = Array.from(document.querySelectorAll("form input"));
-const labels = Array.from(document.querySelectorAll("label"));
-const errorBoxes = Array.from(document.querySelectorAll(".error-box"));
 const submitButton = document.getElementById("form-but")
+const noError = true;
 
-
-// Prevent Default Form Submission
-function formFunc(event) {
-    event.preventDefault()
+// Flag Invalid Char
+const invalidInput = (regex, input) => {
+    return regex.test(input);
 }
 
-form.addEventListener("submit", formFunc)
+
+function triggerError(label, input, errorbox, message) {
+    label.style.color = "red";
+    input.style.borderColor = "red";
+    errorbox.innerHTML = `<p style="color: red" class="error-text">${message}</p>`;
+    noError = false;
+}
 
 
-// Submit form
+function checkForSpacesInInputs(labels, userInputs, errorBoxes) {
+    for (let i = 0; i < labels.length; i++) {
+         const userInput = userInputs[i];
+         const label = labels[i];
+         const errorBoxes = erroroBoxes[i]
+
+        if (invalidInput(/\s/g, userInput.value)) {
+            triggerError(userInput, label, errorBoxes, "Error! No spaces allowed in field.")
+        }
+    }
+}
+
+
+function validateEmailAddress(emailInput, emailLabel, emailErrorField) {
+    const emailAddress = emailInput.value
+
+    if (emailAddress.includes("@")) {
+        const usernameAndDomain = emailAddress.split("@");
+        const emailUsername = usernameDomain[0];
+        const emailDomain = usernameDomain[1];
+
+        // Prompts Username Error
+        if (invalidEmail(/^[.-_!#$%&'*+/=?^`{|}~]|[.-_!#$%&'*+/=?^`{|}~]$|[\\,":;<>[\]()]|[-_!\.#$%&'*+/=?^`{|}~]{2}/, username)) {
+            triggerError(emailLabel, emailInput, emailErrorField, "Error! in email username!!!");
+        }
+
+        // Prompt Domain Error
+        if (invalidEmail(/^[-.]|[-.]$|[^a-zA-Z0-9-.]|[.-]{2}/, domain)) {
+            triggerError(emailLabel, emailInput, emailErrorField, "Error! in email domain!!!");
+        }
+    else {
+        noError = false;
+    }
+}
+
+
 function submitForm() {
-    form.submit()
+    // Labels
+    const labels = document.querySelectorAll("label");
+    const userInputs = document.querySelectorAll("input");
+    const errorFields = document.querySelectorAll(".error-field");
+
+    // Detect Spaces in fields
+    checkForSpaceError(labels, userInputs, errorFields);
+
+    // Validate Email Address
+    validateEmailAddress(userInput[1], labels[1], errorFields[1])
+
+    if (noError) {
+        form.submit()
+    }
 }
 
 submitButton.addEventListener("click", submitForm)
@@ -24,42 +75,14 @@ submitButton.addEventListener("click", submitForm)
 // Ctrl + Enter Form Submission
 function ctrlEnterSubmit(event) {
     if (event.code === "Enter") {
-        event.preventDefault();
-        form.submit();
+        submitForm();
     }
 }
 
 document.addEventListener("keyup", ctrlEnterSubmit)
 
 
-// Flag Invalid Char
-function isInvalidInput(input, regex) {
-    const userInput = input.value
-    return regex.test(userInput)
-}
 
-
-function detectSpacesInFields() {
-    // Detect Spaces in Input Fields
-    for (let i = 0; i < labels.length; i++) {
-        const userInput = userInputs[i];
-        const label = labels[i];
-        const errorBox = errorBoxes[i];
-        if (isInvalidInput(element, /\s/)) {
-            userInput.style.borderColor = "red";
-            label.style.color = "red";
-            errorBox.innerText = `Error! Spaces in the ${label.innerText} field.`;
-            return true;
-        }
-    }
-}
-
-
-function detectInvalidChar() {
-    const userInput = userInputs[1];
-    const userLabel = labels[1];
-    const errorBox = errorBoxes[1]
-}
 
 
 
