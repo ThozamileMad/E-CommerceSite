c37a91abfd22d627ab08399139720ad4897a2734
ThozamileMad <118363197+ThozamileMad@users.noreply.github.com>
2024-07-20 01:34:25 +0200

    Update sign_up_scripts.js

--- static/scripts/sign_up_scripts.js
+++ static/scripts/sign_up_scripts.js
@@ -1,5 +1,6 @@
 const form = document.getElementById("form")
 const submitButton = document.getElementById("form-but")
+const validEmail = false
 
 
 // Flag Invalid Char
@@ -22,23 +23,64 @@ function triggerValid(label, input, errorbox) {
 }
 
 
-function checkForSpacesInInputs(labels, userInputs, errorBoxes) {
-    let validInputNum = 0
-    for (let i = 0; i < labels.length; i++) {
-        const userInput = userInputs[i];
-        const userInputValue = userInput.value
-        const label = labels[i];
-        const errorBox = errorBoxes[i]
-
-        // Checks For
-        if (invalidInput(/\s/g, userInputValue.trim()) || userInputValue.length === 0 || invalidInput(/^\s/, userInputValue)) {
-            triggerError(label, userInput, errorBox, "Error!!! No spaces allowed in field.");
-            validInputNum += 1;
-        }  else {
-            triggerValid(label, userInput, errorBox);
+function matchPasswordChar() {
+    // Array.from(userInput.match(/[A-Z]/g)).length
+    const regexList = [/[A-Z]/g, /[a-z]/g, /[\d]/g, /[!@#$%^&*(),.?":{}|<>_\-+=~`[\]\\/'\\;]/g];
+    let numOfMatches = [];
+    for (let i = 0; i < regexList.length; i++) {
+        console.log(userInput.match(regexList[i]))
+        if (userInput.match(regexList[i]) !== null) {
+            numOfMatches.push(userInput.match(regexList[i]).length)
+        } else {
+            numOfMatches.push(0);
         }
     }
-    return validInputNum === 3 ? true : false;
+    return numOfMatches
+}
+
+
+function checkPasswordStrength() {
+    const matches = matchPasswordChar();
+    if (matches[0] )
+}
+
+function usernameValidator() {
+    const label = document.querySelector("label[for='username']");
+    const input = document.getElementById("username");
+    const inputValue = input.value;
+    const inputLength = inputValue.trim().length;
+    const errorField = document.querySelector(".error-box.username");
+
+    if (invalidInput(/[^a-zA-Z\d_\-.]/, inputValue.trim()) || invalidInput(/^\s/, inputValue)) {
+        triggerError(label, input, errorField, `Invalid username: Please use only letters, numbers, underscores (_), hyphens (-), and periods (.).`);
+    } else if (inputLength < 3) {
+        triggerError(label, input, errorField, "Your username must be at least 3 characters long. Please enter a longer username.")
+    } else if (inputLength > 12) {
+        triggerError(label, input, errorField, "Your username must not exceed 12 characters. Please enter a shorter username.")
+    } else {
+        triggerValid(label, input, errorField);
+        return true;
+    }
+}
+
+
+function passwordValidator() {
+    const label = document.querySelector("label[for='password']");
+    const input = document.getElementById("password");
+    const inputValue = input.value;
+    const inputLength = inputValue.trim().length;
+    const errorField = document.querySelector(".error-box.password");
+
+    if (invalidInput(/\s/g, inputValue.trim()) || invalidInput(/^\s/, inputValue)) {
+        triggerError(label, input, errorField, "Invalid Password: Your password contains spaces, please remove all spaces.");
+    } else if (inputLength < 8) {
+        triggerError(label, input, errorField, "Invalid Password: Your password must be at least 8 characters long. Please enter a longer password.")
+    } else if (inputLength > 64) {
+        triggerError(label, input, errorField, "Invalid Password: Your password must not exceed 64 characters. Please enter a shorter password.")
+    } else {
+        triggerValid(label, input, errorField);
+        return true
+    }
 }
 
 
@@ -50,9 +92,9 @@ function validateEmailAddress(emailInput, emailLabel, emailErrorField) {
         const emailDomain = usernameAndDomain[1];
 
         // Prompts Username and Domain Error
-        if (invalidInput(/^[\.\-_!#$%&'*+/=?^`{|}~]|[\.\-_!#$%&'*+/=?^`{|}~]$|[\\,":;<>[\]()]|[-_!\.#$%&'*+/=?^`{|}~]{2}/, emailUsername)) {
+        if (invalidInput(/^[\.\-_!#$%&'*+/=?^`{|}~\s]|[\.\-_!#$%&'*+/=?^`{|}~]$|[\\,":;<>[\]()\s]|[-_!\.#$%&'*+/=?^`{|}~]{2}/, emailUsername)) {
             triggerError(emailLabel, emailInput, emailErrorField, "Error!!! Invalid Username.");
-        } else if (invalidInput(/^[-.]|[-.]$|[^a-zA-Z0-9-.]|[.-]{2}/, emailDomain)) {
+        } else if (invalidInput(/^[-.\s]|[-.]$|[^a-zA-Z0-9-.]|[.-]{2}/, emailDomain)) {
             triggerError(emailLabel, emailInput, emailErrorField, "Error!!! Invalid Domain.");
         } else {
             triggerValid(emailLabel, emailInput, emailErrorField);
@@ -70,13 +112,12 @@ function submitForm() {
     const userInputs = document.querySelectorAll("input");
     const errorFields = document.querySelectorAll(".error-box");
 
-    // Detect Spaces in fields
-    const spacesInInputs = checkForSpacesInInputs(labels, userInputs, errorFields);
-
     // Validate Email Address
+    const usernameIsValid = usernameValidator()
     const emailIsValid = validateEmailAddress(userInputs[1], labels[1], errorFields[1])
+    passwordIsValid = passwordValidator()
 
-    if (spacesInInputs === false && emailIsValid) {
+    if (usernameIsValid && emailIsValid && passwordIsValid) {
         form.submit()
     }
 }
